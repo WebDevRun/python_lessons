85400a18fcdb87cb703d0625df5e47c41e8d3367
Leonid <leonkaram@mail.ru>
2022-04-05 22:54:36 +0700

    lint dictionary

--- variables/dictionary/2_dictionary_methods.py
+++ variables/dictionary/2_dictionary_methods.py
@@ -1,52 +1,52 @@
-""""Методы словарей."""
+"""Методы словарей."""
 
 print('Методы словарей')
 
 gender = {
-  0: 'man',
-  1: 'woman'
+    0: 'man',
+    1: 'woman'
 }
 
 main_dictionary = {
-  'admin': [
-    {
-      'id': 1,
-      'name': 'Alex',
-      'nickname': 'aleks734',
-      'password': '123456',
-      'gender': gender[0]
-    },
-    {
-      'id': 2,
-      'name': 'Elena',
-      'nickname': 'elen777',
-      'password': '349323',
-      'gender': gender[1]
-    }
-  ],
-  'user': [
-    {
-      'id': 3,
-      'name': 'Timur',
-      'nickname': 'tim544',
-      'password': '34578234',
-      'gender': gender[0]
-    },
-    {
-      'id': 4,
-      'name': 'Rick',
-      'nickname': 'rick341',
-      'password': '23496512',
-      'gender': gender[0]
-    },
-    {
-      'id': 5,
-      'name': 'Sam',
-      'nickname': 'sam111',
-      'password': '8966432',
-      'gender': gender[0]
-    }
-  ]
+    'admin': [
+        {
+            'id': 1,
+            'name': 'Alex',
+            'nickname': 'aleks734',
+            'password': '123456',
+            'gender': gender[0]
+        },
+        {
+            'id': 2,
+            'name': 'Elena',
+            'nickname': 'elen777',
+            'password': '349323',
+            'gender': gender[1]
+        }
+    ],
+    'user': [
+        {
+            'id': 3,
+            'name': 'Timur',
+            'nickname': 'tim544',
+            'password': '34578234',
+            'gender': gender[0]
+        },
+        {
+            'id': 4,
+            'name': 'Rick',
+            'nickname': 'rick341',
+            'password': '23496512',
+            'gender': gender[0]
+        },
+        {
+            'id': 5,
+            'name': 'Sam',
+            'nickname': 'sam111',
+            'password': '8966432',
+            'gender': gender[0]
+        }
+    ]
 }
 
 print(f'main_dictionary = {main_dictionary}')
@@ -57,7 +57,7 @@ copy_dictionary = main_dictionary.copy()
 print(f'copy_dictionary = {copy_dictionary}')
 
 # Метод clear() очищает словарь
-copy_dictionary = copy_dictionary.clear()
+copy_dictionary.clear()
 
 print(f'copy_dictionary = {copy_dictionary}')
 
@@ -100,8 +100,8 @@ print(f'fromkeys_dict = {fromkeys_dict}')
 # Метод update([other]) обновляет словарь,
 # добавляя пары (ключ, значение) из other.
 # Существующие ключи перезаписываются. Возвращает None.
-fromkeys_dict.update({'five': 5}) 
-fromkeys_dict.update({'four': 4}) 
+fromkeys_dict.update({'five': 5})
+fromkeys_dict.update({'four': 4})
 
 print(f'fromkeys_dict = {fromkeys_dict}')
 
